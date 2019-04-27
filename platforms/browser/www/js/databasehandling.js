617002c04d7d141681fc1ee991037e1db0d341c4
Yoofi96 <joseph.brown-pobee@ashesi.edu.gh>
2019-04-27 02:06:33 +0000

    Updates

---
+++ platforms/browser/www/js/databasehandling.js
@@ -0,0 +1,70 @@
+var db = firebase.firestore();
+//$('#topicButton').click(listVideos($('#anotherSelect :selected').val()));
+$('#anotherSelect').change(function(){
+  $('.videos').empty();
+  var topic  = $('#anotherSelect :selected').text();
+  db.collection("videos").where("title", "==", topic).get().then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+          $(".videos").append(doc.data().link);
+          $(".videos").append(doc.data().title);
+      });
+  })
+})
+
+$('#add_entry').click(function(){
+  var title = $('#journal_title').val();
+  var content = $('#journal_post').val();
+  if(title == "" || content ==""){
+    alert("Enter the information");
+  }else{
+  var user = firebase.auth().currentUser;
+  var imageData = $('#imageAttachments').attr('src');
+  if(imageData!=undefined){
+  var imagebaseName = baseName(imageData);
+  var getFileBlob = function(url, cb) {
+         var xhr = new XMLHttpRequest();
+         xhr.open("GET", url);
+         xhr.responseType = "blob";
+         xhr.addEventListener('load', function() {
+             cb(xhr.response);
+         });
+         xhr.send();
+     };
+
+     var blobToFile = function(blob, name) {
+         blob.lastModifiedDate = new Date();
+         blob.name = name;
+         return blob;
+     };
+
+     var getFileObject = function(filePathOrUrl, cb) {
+          getFileBlob(filePathOrUrl, function(blob) {
+              cb(blobToFile(blob, 'test.jpg'));
+          });
+      };
+
+      getFileObject(imageData, function(fileObject) {
+          var uploadTask = storageRef.child('images/'+imagebaseName).put(fileObject);
+
+          uploadTask.on('state_changed', function(snapshot) {
+              console.log(snapshot);
+          }, function(error) {
+              console.log(error);
+          }, function() {
+              var downloadURL = uploadTask.snapshot.downloadURL;
+              console.log("downloadURL" + downloadURL);
+              // handle image here
+          });
+      });
+    }
+  db.collection('entries').add({
+    title: $('#journal_title').val(),
+    content: $('#journal_post').val(),
+    userdisplayName: user.displayName
+  }).then(function(docRef){
+    window.location.hash = '#journal';
+    window.location.reload(true);
+  }).catch(function(error){
+    console.log(error);
+  });
+}})
---
+++ platforms/browser/www/js/userhandling.js
@@ -0,0 +1,121 @@
+function signIn(){
+var provider = new firebase.auth.GoogleAuthProvider();
+
+firebase.auth().signInWithPopup(provider).then(function(result){
+  var token = result.credential.accessToken;
+  var user = result.user;
+  window.location.href = '#settings';
+}).catch(function(error){
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  var email = error.email;
+  var credential = error.credential;
+});
+}
+
+function signOut(){
+  firebase.auth().signOut().then(function(){
+    console.log("Signed Out First");
+  }).catch(function(error){
+    console.log("Failed");
+  });
+}
+
+function isUserSignIn(){
+  return !!firebase.auth().currentUser;
+}
+
+function validateEmail(){
+  var email = $("#email").val();
+  var emailError = "";
+  if(email.length == 0 || !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)){
+    emailError+="Please enter a valid email\n";
+  }
+    return emailError;
+}
+
+var constraints = {
+  password: {
+    presence: true,
+    length: {
+      minimum: 6,
+      message: "must be at least 6 characters"
+    }
+  }
+}
+
+function validateSignUpPassword(){
+  var user_password = $("#password").val();
+  var result = validate({password: user_password}, constraints)
+  console.log(result);
+  alert(result.password);
+}
+
+
+function validatePassword(){
+  var password = $("#password").val();
+  var passwordError = "";
+  if(password.length == 0){
+    passwordError+="Please enter a password\n";
+  }
+    return passwordError;
+}
+
+function validateLogin(){
+  var loginError = "";
+  var password = $("#password").val();
+  var email = $("#email").val();
+  loginError += validatePassword();
+  loginError += validateEmail();
+  if(loginError != ""){
+    console.log(loginError);
+    navigator.notification.alert(loginError, function(){}, "Invalid Login Details");
+  }else{
+    firebase.auth().signInWithEmailAndPassword(email,password).then(function(){window.location.href = '#settings'}).catch(function(error){
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // console.log(errorCode);
+      // console.log(errorMessage);
+    });
+  }
+}
+
+function signUp(){
+  var signUpError = "";
+  var displayname = $('#first_name').val() + " " + $('#last_name').val();
+  var email = $('#sign_email').val();
+  signUpError+=validateEmail();
+  var password = $('#sign_password').val();
+  var confirmpassword = $('#confirm_password').val();
+  firebase.auth().createUserWithEmailAndPassword(email, password).then(pageTwo).catch(function(error){
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    console.log(errorCode);
+    console.log(errorMessage);
+  })
+}
+
+ function setUsername(){
+   var user = firebase.auth().currentUser;
+   var name = $('#username').val();
+   user.updateProfile({
+     displayName : name
+   }).then(function(){
+     window.location.hash = '#settings';
+     window.location.reload(true);
+   }).catch(function(error){});
+ }
+
+ /*function setProfilePicture(){
+   var user = firebase.auth().currentUser;
+   var name = $('#imageAttachments').attr('src');
+   user.updateProfile({
+     photoURL : name
+   }).then(pageThree).catch(function(error){});
+ }*/
+
+$("#signUpButton").click(signUp);
+$('#usernameButton').click(setUsername);
+$("#loginButton").click(validateLogin);
+$("#googleButton").click(signIn);
+$("#signOut").click(signOut);
